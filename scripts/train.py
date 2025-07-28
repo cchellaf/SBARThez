8b833b03e808b99de1cf07cf843f21e1e5044f88
Chaimae CHELLAF EL HAMMOUD <chellaf.chaimae@ad.lundimatin.fr>
2025-07-28 13:12:52 +0200

    Initial commit: add folder structure

---
+++ scripts/train.py
@@ -0,0 +1,4 @@
+from dataset.kaldi_dataset import KaldiDataset, collate_fn
+from models.sbarthez_model import SBARThez_BGE
+from utils.generation_utils import generate_summary
+# Then your main loop
